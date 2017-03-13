bac9785fa97ed7224cb64e76390db7b719dc8e37
cdgrady21 <cdgrady2@illinois.edu>
2017-03-12 20:53:47 -0500

    Plot function works yaaayyy!!

--- cp3_data_analysis(3).R
+++ cp3_data_analysis(3).R
@@ -8,6 +8,13 @@ library(stats)
 library(ggplot2)
 library(reshape2)
 
+###############
+# Again re-scale everything to be 0-1
+###############
+cpdf<-as.data.frame(lapply(cpdf,scale.fun)) # already tested function in (1) script
+#typetest2=sapply(test,is.numeric)
+#stopifnot(table(test$vio.index)['1']==table(cpdf$vio.index)['4'])
+
 ###########
 # Survey Corrections
 ###########
@@ -67,8 +74,8 @@ svylm.fun<-function(outcome,treatment,data=svy)
 ####################################
 # # Easily get mean & Standard Error of survey vars
 ## plot.vars will be fed into this.
-plot.vars<-c("gender", 'adult', 'religion', 'region', 
-             'form.End.survey_language','vio.index')
+#plot.vars<-c("gender", 'adult', 'religion', 'region', 
+#             'form.End.survey_language','vio.index')
 meanse.fun<-function(outcome,vars,df)
 {
   df1<-svyby(reformulate(outcome),
@@ -105,59 +112,60 @@ meanse.fun<-function(outcome,vars,df)
   return(final)
 }
 #mean.df<-meanse.fun('vio.index',plot.vars,svy)
-
+#mean.df<-meanse.fun('form.Attitudes.corruption_problem',plot.vars,svy)
 
 ########
 # Table-making function
 #######
-plot.fun<-function(outcome,data)
+plot.fun<-function(outcome,dat,title)
 {
+  # set the vars of the plot dataset
   plot.vars<-c("gender", 'adult', 'religion', 'region', 
                'form.End.survey_language',outcome)
-  df<-meanse.fun(outcome,plot.vars,data)
-  return(df)
+  
+  # from function above.
+  # grab mean & se of those vars, accounting for survey parameters.
+  df1<-meanse.fun(outcome,plot.vars,dat)
+  
+  # Format the data for ggplot
+  mean.df<-reshape2::melt(df1,id.vars=c(outcome,'se','subset'))[,-4] # drop redundant "variable" column.
+  mean.df$subset<-as.factor(mean.df$subset)
+  levels(mean.df$subset)<-c('Age',"Language","Gender","Region","Religion")
+  mean.df$subset<-factor(mean.df$subset, levels(mean.df$subset)[c(3,1,4,5,2)])
+  
+  # calculate ymin and ymax for error bars
+  miner<-mean.df[[outcome]]-mean.df$se
+  maxer<-mean.df[[outcome]]+mean.df$se
+
+  # Make the plot
+  ggplot(mean.df, aes(x=subset, y=mean.df[[outcome]], fill=value)) + 
+    geom_bar(position=position_dodge(0.9), stat="identity",
+             colour="black", # Use black outlines,
+             size=.5) +      # Thinner black lines
+    geom_errorbar(aes(ymin=miner, ymax=maxer),
+                  size=.3,    # Thinner lines
+                  width=.2,
+                  position=position_dodge(.9)) +
+    xlab("Respondent Subsets") +
+    ylab("Outcome Score") +
+    scale_fill_hue(name="Group", # Legend label, use darker colors
+                   breaks=c('female','male','Adult','Youth','exno','no',
+                            'christian','islam','other','survey_lang_fr',
+                            'survey_lang_ful','survey_lang_other'),
+                   labels=c('Female','Male','Adult','Youth','Ex North','North',
+                            'Christian','Muslim','Other Rel','French',
+                            'Fulfulde','Other Lang')) +
+    ggtitle(title) +
+    #scale_y_continuous(limit=c(0,1)) +
+    theme_bw() +
+    theme(panel.grid.major = element_blank()) +
+    theme(plot.title = element_text(size=18))
+  
 }
-plot.fun(outcome='vio.index',data=svy)
-
-
-# Gosh darnit
-plot.vars<-c("gender", 'adult', 'religion', 'region', 'form.End.survey_language','vio.index')
-df.test<-meanse.fun('vio.index',plot.vars,svy)
-mean.df<-reshape2::melt(df.test,id.vars=c('vio.index','se','subset'))[,-4] # drop redundant "variable" column.
-mean.df$subset<-as.factor(mean.df$subset)
-levels(mean.df$subset)<-c('Age',"Language","Gender","Region","Religion")
-mean.df$subset<-factor(mean.df$subset, levels(mean.df$subset)[c(3,1,4,5,2)])
-tally(mean.df$value) # could re-order this?
-
-# my survey exp paper
-ggplot(mean.df, aes(x=subset, y=vio.index, fill=value)) + 
-  geom_bar(position=position_dodge(0.9), stat="identity",
-           colour="black", # Use black outlines,
-           size=.5) +      # Thinner black lines
-  geom_errorbar(aes(ymin=vio.index-se, ymax=vio.index+se),
-                size=.3,    # Thinner lines
-                width=.2,
-                position=position_dodge(.9)) +
-  xlab("Respondent Subsets") +
-  ylab("Violence Index Score") +
-  scale_fill_hue(name="Group", # Legend label, use darker colors
-                 breaks=c('female','male','Adult','Youth','exno','no',
-                          'christian','islam','other','survey_lang_fr',
-                          'survey_lang_ful','survey_lang_other'),
-                 labels=c('Female','Male','Adult','Youth','Ex North','North',
-                          'Christian','Muslim','Other Rel','French',
-                          'Fulfulde','Other Lang')) +
-  ggtitle("Violence Index by Group") +
-  scale_y_continuous(limit=c(0,4)) +
-  theme_bw() +
-  theme(panel.grid.major = element_blank()) +
-  theme(plot.title = element_text(size=18))
-
-
-
-
-
-
+plot.fun(outcome="vio.index",
+         dat=svy,title="Violence Index by Group")
+plot.fun(outcome='form.Attitudes.corruption_problem',dat=svy,
+         title="Corruption Belief by Group")
 
 
 #########################
@@ -170,11 +178,3 @@ stopifnot(mean(cpdf[['vio.index']][cpdf$religion %in% "christian"],na.rm=T)==
 qplot(variable, vio.index, data=mean.df, geom=c("boxplot", "jitter"), 
       fill=variable, main="vio.index", ylim=c(3,4),
       xlab="", ylab="vio.index")
-
-
-
-
-##################
-# Look at stuff systematically
-#################
-
